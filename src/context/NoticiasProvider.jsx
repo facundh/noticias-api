7779ad0028e21221d7d2056141a9ba13d2f62c1b
Facundo Herrera <facundhfed@gmail.com>
2022-05-21 23:20:59 +0200

    peticiones realizadas correctamente

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,9 +1,23 @@
+import axios from "axios";
 import { useState, useEffect, createContext } from "react";
 
 export const NoticiasContext = createContext();
 
 const NoticiasProvider = ({children}) => {
     const [categorias, setCategorias] = useState('general');
+    const [noticias, setNoticias] = useState([]);
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=ar&vategory=${categorias}&apiKey=${import.meta.env.VITE_API_KEY}`
+
+            const { data } = await axios(url);
+
+           setNoticias(data.articles);
+        }
+
+        consultarAPI()
+    },[categorias])
 
     const handleChangeCategoria = e => {
         setCategorias(e.target.value);
